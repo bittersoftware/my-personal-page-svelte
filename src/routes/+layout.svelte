4cc151740540439c6d79328dbd29693955b4dc92
Thiago Bittencourt <bittersoftware@gmail.com>
2023-07-21 03:06:08 +0200

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-gold-nouveau.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// Most of your app wide CSS should be put in this file
+	import Navbar from '$lib/components/Navbar.svelte';
+	import { AppShell, autoModeWatcher } from '@skeletonlabs/skeleton';
+	import '../app.postcss';
+</script>
+
+<svelte:head
+	>{@html `<script>${autoModeWatcher.toString()} autoModeWatcher();</script>`}</svelte:head
+>
+
+<AppShell>
+	<svelte:fragment slot="pageHeader" />
+	<div class="sm:block">
+		<Navbar />
+	</div>
+	<slot />
+	<!-- (pageFooter) -->
+	<svelte:fragment slot="footer">Footer</svelte:fragment>
+</AppShell>
