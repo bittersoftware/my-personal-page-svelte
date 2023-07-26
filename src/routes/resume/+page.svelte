f863fe27e8928254f62e3752e02ded48868b2e01
Thiago Bittencourt <bittersoftware@gmail.com>
2023-07-27 01:19:32 +0200

    resume draft

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,53 +1,135 @@
-Resume
 <svelte:head>
 	<link rel="stylesheet" href="https://unpkg.com/mono-icons@1.0.5/iconfont/icons.css" />
 </svelte:head>
 
+<p>
+	I work with Quality Assurance since 2012. I have started with of automotive embedded systems then
+	moved to telecommunications and video engineering.
+</p>
+<p>
+	I have experience with requirement analysis, test management, test plan creation, test execution,
+	test automation and troubleshooting.
+</p>
+<p>I am Brazilian, currently working as Automation Lead in Madrid, Spain</p>
+
+<h2>Experience</h2>
+
 <section>
-	<div class="mb-5 flex items-start">
-		<img
-			src="images/max.png"
-			alt="Avatar"
-			class="h-14 w-14 shrink-0 rounded-xl border-2 border-gray-50 shadow-sm dark:border-night-700"
-		/>
-		<div class="ml-3 w-full space-y-5">
-			<div class="justify-between sm:flex">
-				<div class="space-y-2">
-					<div class="font-medium dark:text-night-50">JOB</div>
-					<div class="space-y-1 sm:flex sm:space-x-5">
-						<div
-							class="flex items-center gap-1 text-sm font-medium text-gray-400 dark:text-night-400"
-						>
-							<i class="mi mi-add"><span class="u-sr-only h-4 w-4 shrink-0">Add</span></i>
-							<span>COMPANY</span>
-						</div>
-						<div
-							class="flex items-center gap-1 text-sm font-medium text-gray-400 dark:text-night-400"
-						>
-							<i class="mi mi-add"><span class="u-sr-only h-4 w-4 shrink-0">Add</span></i>
-							<span>LOCATION</span>
-						</div>
-					</div>
+	<ul>
+		<li class="px-4 sm:mx-12 sm:py-4 sm:w-8/12">
+			<div class="flex gap-x-4">
+				<img class="h-12 w-12 flex-none rounded" src="images/company/wbd.jpeg" alt="" />
+				<div class="flex-auto">
+					<h3 class="text-xl font-semibold leading-6">Warner Bros. Discovery</h3>
+					<p class="text-md mt-1 leading-5 text-gray-500">Aug - 2021 - Present</p>
 				</div>
-				<div class="flex items-start justify-between space-y-1 sm:block sm:text-right">
-					<div
-						class="order-last inline-flex items-center rounded-lg bg-primary-500/20 px-2 py-1 text-xs font-medium text-primary-500"
-					>
-						WORKTYPE
+			</div>
+			<div class="flex gap-x-4 py-4">
+				<div class="">
+					<div class="flex gap-x-4">
+						<p class="text-lg font-semibold leading-6 pb-2">Automation Lead</p>
+						<p class=" text-gray-500">Oct-2022 - Present</p>
+						<span><i class="mi mi-location" /> Madrid - Spain</span>
 					</div>
-					<div
-						class="flex items-center gap-1 text-sm font-medium text-gray-400 dark:text-night-400"
-					>
-						<i class="mi mi-add"><span class="u-sr-only h-4 w-4 shrink-0">Add</span></i>
-						<span>START - END</span>
+					<ul class="pb-2">
+						<li>
+							<span>
+								• Leading the test automation team across different countries for the Max and HBO
+								Max projects, focusing on various models of TV decoders, Android TV, and Fire TV
+								devices.
+							</span>
+						</li>
+						<li>
+							<span>
+								• Developing test automation frameworks to facilitate multi–device support with a
+								unified code base. Conducting onboarding sessions and delivering training to the
+								Discovery+ team on utilizing the automation stack effectively.
+							</span>
+						</li>
+					</ul>
+					<div class="flex gap-x-2 pt-4">
+						<svg
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-6 h-6"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+							/>
+						</svg>
+
+						<div class="flex gap-x-2">
+							<span class="chip variant-soft-surface">Teamwork</span>
+							<span class="chip variant-soft-surface">Test case management</span>
+							<span class="chip variant-soft-surface">Troubleshooting</span>
+							<span class="chip variant-soft-surface">Decision making</span>
+							<span class="chip variant-soft-surface">Communication</span>
+						</div>
+					</div>
+
+					<div class="flex gap-x-2 pt-3">
+						<svg
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-6 h-6"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z"
+							/>
+						</svg>
+						<span class="chip variant-soft-surface">Python</span>
+						<span class="chip variant-soft-surface">Team Lead</span>
+						<span class="chip variant-soft-surface">Open Computer Vision</span>
+						<span class="chip variant-soft-surface">Pytest</span>
+						<span class="chip variant-soft-surface">BDD</span>
+						<span class="chip variant-soft-surface">Git</span>
+						<span class="chip variant-soft-surface">Git Actions</span>
+						<span class="chip variant-soft-surface">API integration</span>
 					</div>
 				</div>
 			</div>
-			<p class="text-sm text-gray-600 dark:text-night-300">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed finibus est vitae tortor
-				ullamcorper, ut vestibulum velit convallis. Aenean posuere risus non velit egestas suscipit.
-			</p>
-			<div class="border-b border-dashed border-gray-200 dark:border-night-600" />
-		</div>
-	</div>
+			<div class="flex gap-x-4 py-4">
+				<div class="">
+					<div class="flex gap-x-4">
+						<p class="text-lg font-semibold leading-6 pb-2">SDET</p>
+						<p class=" text-gray-500">Aug-2021 - Sep-2022</p>
+						<span><i class="mi mi-location" /> Madrid - Spain</span>
+					</div>
+					<ul class="pb-2">
+						<li>
+							<span>
+								• Leading the test automation team across different countries for the Max and HBO
+								Max projects, focusing on various models of TV decoders, Android TV, and Fire TV
+								devices.
+							</span>
+						</li>
+						<li>
+							<span>
+								• Developing test automation frameworks to facilitate multi–device support with a
+								unified code base. Conducting onboarding sessions and delivering training to the
+								Discovery+ team on utilizing the automation stack effectively.
+							</span>
+						</li>
+					</ul>
+					<div class="flex">
+						<p class="pr-2 text-gray-500">Skills:</p>
+						<p class="">Python, Open Computer Vision, Pytest, BDD, Git Actions</p>
+					</div>
+					<div class="flex">
+						<p class="pr-2 text-gray-500">Skills:</p>
+						<p class="">Python, Open Computer Vision, Pytest, BDD, Git Actions</p>
+					</div>
+				</div>
+			</div>
+			<hr />
+		</li>
+	</ul>
 </section>
