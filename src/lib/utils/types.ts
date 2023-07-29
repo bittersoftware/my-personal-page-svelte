6d4685c0279ac53379575f5ae07d252f8ba6eb5a
Thiago Bittencourt <bittersoftware@gmail.com>
2023-07-29 21:53:32 +0200

    job description component draft

--- src/lib/utils/types.ts
+++ src/lib/utils/types.ts
@@ -8,3 +8,23 @@ export type Language = {
     flag: string;
     language: string;
 }
+
+export type JobPosition = {
+    positionTitle: string;
+    startTime: Date;
+    endTime: Date | string;
+    location: string;
+    description: string;
+    skills: string;
+    stack: string;
+
+}
+
+export type JobDescription = {
+    companyName: string;
+    imageLogo: string;
+    workType: string;
+    startTime: Date;
+    endTime: Date | string;
+    position: JobPosition[];
+}
--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -14,7 +14,7 @@
 
 <h2>Experience</h2>
 
-<section>
+<div>
 	<div class="flex">
 		<div class="col-1 px-4">
 			<div class="w-14">
@@ -23,23 +23,45 @@
 		</div>
 		<div class="col-2 h-14">
 			<div class="mb-4">
-				<h4 class="h4">Warner Bros Discovery</h4>
+				<h4 class="text-xl">Warner Bros Discovery</h4>
 				<p>Full Time 2y</p>
 			</div>
 			<div class="">
 				<ul>
 					<li>
-						<div class=" relative">
-							<span class="-left-12 top-4 relative block rounded-full h-2 w-2 bg-gray-500" />
-							<p class="position-title font-bold">Automation Lead</p>
-							<span class="-left-11 absolute w-0.5 bg-gray-500 h-5/6" />
-							<p class="text-gray-500">Oct 22 - Present · 10 months</p>
-							<p class="text-gray-500">Madrid, Spain</p>
-							<p>
+						<div class="relative">
+							<span class="-left-[45px] absolute w-0.5 bg-surface-500 h-full mt-6" />
+							<span class="-left-12 top-4 relative block rounded-full h-2 w-2 bg-surface-500" />
+							<p class=" font-bold text-lg">Automation Lead</p>
+							<div class="pb-2">
+								<p class="text-gray-500">Oct 22 - Present · 10 months</p>
+								<div class="flex">
+									<svg
+										class="flex-none w-6 h-6 fill-surface-500 stroke-none"
+										viewBox="4 0 24 24"
+										fill="none"
+										><g id="SVGRepo_bgCarrier" stroke-width="0" /><g /><g id="SVGRepo_iconCarrier">
+											<path
+												d="M12 2C7.58172 2 4 6.00258 4 10.5C4 14.9622 6.55332 19.8124 10.5371 21.6744C11.4657 22.1085 12.5343 22.1085 13.4629 21.6744C17.4467 19.8124 20 14.9622 20 10.5C20 6.00258 16.4183 2 12 2Z"
+											/>
+											<path
+												d="M12 12.5C13.3807 12.5 14.5 11.3807 14.5 10C14.5 8.61929 13.3807 7.5 12 7.5C10.6193 7.5 9.5 8.61929 9.5 10C9.5 11.3807 10.6193 12.5 12 12.5Z"
+												fill="#fff"
+											/>
+										</g></svg
+									>
+									<p class="text-gray-500">Madrid, Spain</p>
+								</div>
+							</div>
+							<p class="py-2">
 								Automação de testes no projeto HBO Max <br />
 								- stb-tester: OpenCV, Tesseract, Page Object Model <br />
 								- Dispositivos: Sagemcom, Comcast, Nvidia Shield, Roku Stick
 							</p>
+							<div class="pb-4">
+								<p><strong>Skills: </strong>Team lead</p>
+								<p><strong>Tech Stack: </strong>Python, Open CV, Github Actions</p>
+							</div>
 						</div>
 					</li>
 					<li>
@@ -57,4 +79,4 @@
 			</div>
 		</div>
 	</div>
-</section>
+</div>
